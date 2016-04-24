9cfb4f34e849edcf368724782f66f9b58e8be1a3
codingisall <leogee@126.com>
2016-04-25 00:44:14 +0800

    Add wash command

--- src/com/bluet/ui/CellSaverControl.java
+++ src/com/bluet/ui/CellSaverControl.java
@@ -14,6 +14,9 @@ import com.bluet.massistant.R;
 public class CellSaverControl extends BaseFragment {
 	Button fill;
 	byte[] message_fill = new byte[8];
+	
+	Button wash;
+	byte[] message_wash = new byte[8];
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.cell_saver_control, null, false);
@@ -30,6 +33,21 @@ public class CellSaverControl extends BaseFragment {
 				message_fill[6] = (byte) 0xCC;
 				message_fill[7] = (byte) 0x33;
 				sendMessage(message_fill);
+			}
+		});
+        
+        wash = (Button) view.findViewById(R.id.wash);
+        wash.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				message_wash[0] = (byte) 0x01;
+				message_wash[1] = (byte) 0x07;
+				message_wash[2] = (byte) 0x01;
+				message_wash[3] = (byte) 0x01;
+				message_wash[4] = (byte) 0x09;
+				message_wash[5] = (byte) 0x00;
+				message_wash[6] = (byte) 0xCC;
+				message_wash[7] = (byte) 0x33;
+				sendMessage(message_wash);
 			}
 		});
         
