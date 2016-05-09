18b8620caa9617ee017e41bb31ae4bd2145835ce
zhangfeng <287285415@qq.com>
2016-05-09 22:50:39 +0800

    fix up status_monitor.xml and StatusMonitor.java

--- src/com/bluet/ui/StatusMonitor.java
+++ src/com/bluet/ui/StatusMonitor.java
@@ -27,7 +27,7 @@ public class StatusMonitor extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.status_monitor, null, false);
         Fill_text_view= (TextView) view.findViewById(R.id.fill_ML);
-        Fill_text_view.setText("---Ml");
+        Fill_text_view.setText("65536 mL");
         image_view = (ImageView) view.findViewById(R.id.imageView_status);
         switch(status){
           case 1: //进血
